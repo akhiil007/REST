816081f96c6a5a2b52505cc5b25ff91cf7b33648
Akhil <vc@epaisa.com>
2021-07-23 17:21:54 +0530

    AWS

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 
+//aws
+const config = require('./config.json');
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -14,4 +19,5 @@ app.use(express.json())
 const itemsRouter = require('./routes/item')
 app.use('/items', itemsRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log("Server listening on port " + port))
